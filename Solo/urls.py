780c6febda09ede022fffa2bed28859c346bd784
Fedor <fedor.admidin2010@mail.ru>
2025-02-14 23:26:28 +0300

    Сделал вывод заданий один за другим по task.day формой

--- Solo/urls.py
+++ Solo/urls.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from project import views
+from project.views import complete_day, reset_progress
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='level_up'),
+    path('', views.index, name='index'),
+    path("complete_day/<int:day>/", complete_day, name="complete_day"),
+    path("reset_progress/", reset_progress, name="reset_progress"),
 ]
--- project/views.py
+++ project/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import EverydayTask
 
 def index(request):
-    level =1
-    today = now().day
-    task = EverydayTask.objects.filter(day=today).first()
-    if not task:
-        task = EverydayTask.objects.order_by('day').first()
-    return  render(request, 'index.html', {'level': level, 'task': task})
+    level = 1
+    last_completed_day = request.session.get("last_completed_day", 0)
+    task = EverydayTask.objects.filter(day__gt=last_completed_day).order_by("day").first()
+    
+    return render(request, "index.html", {"task": task, 'level': level})
 
-from django.shortcuts import render
-from .models import EverydayTask
-from datetime import datetime
-from django.utils.timezone import now
+def complete_day(request, day):
+    if request.method == "POST":
+        request.session["last_completed_day"] = day
+        return redirect("index")
 
+    task = EverydayTask.objects.filter(day=day).first()  # Теперь это объект, а не число
+    return render(request, "index.html", {"task": task})
+
+def reset_progress(request):
+    request.session["last_completed_day"] = 0
+    return redirect("index")
